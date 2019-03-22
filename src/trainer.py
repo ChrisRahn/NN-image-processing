87329a262fa09fbc78d2153d994e71c92c01b826
Chris Rahn <crahn2011@gmail.com>
2019-03-22 16:59:19 -0700

    Dummy training and kernel
    Just using triforce.jpg and a hand-made triangular kernel

--- src/trainer.py
+++ src/trainer.py
@@ -5,23 +5,35 @@ Builds the neural network model
 """
 
 from matplotlib.pyplot import imread, imshow
+import numpy as np
 import tensorflow as tf
 from tensorflow import keras
-from tensorflow import initializers
 
 img = imread('../data/triforce.jpg')  # Grey-scale only for now
 
-# Flatten img's values to be [0 - 255]
+# Flatten img's values to be [0 - 1]
 img = img / 255.
 
-# Define a simple kernel
-kernel_t = tf.constant_initializer
+#Set training data to be random and test to be the image
+train_X = np.random.random(size=img.shape)
+train_y = img
+
+# Define a simple triangular kernel
+triangle = np.array([[0, 0, 0, 1, 0, 0, 0],
+                     [0, 0, 1, 1, 1, 0, 0],
+                     [0, 0, 1, 1, 1, 0, 0],
+                     [0, 1, 1, 1, 1, 1, 0],
+                     [0, 1, 1, 1, 1, 1, 0],
+                     [1, 1, 1, 1, 1, 1, 1],
+                     [1, 1, 1, 1, 1, 1, 1]])
+
+kernel_t = tf.constant_initializer(triangle)
 
 # Define a simple LSTM model
 model = keras.Sequential([
         keras.layers.ConvLSTM2D(
                  filters=2,
-                 kernel_size=13,
+                 kernel_size=7,
                  strides=1,
                  dilation_rate=(1, 1),
                  activation='relu',
