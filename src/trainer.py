cb5daf7db184f5d13f93eb38fcfb4fdc347c7f85
Chris Rahn <crahn2011@gmail.com>
2019-03-25 15:39:30 -0700

    Simple Out on Simple In
    Can give a hand-designed input image, train, and predict the original image from random input

--- src/trainer.py
+++ src/trainer.py
@@ -9,7 +9,18 @@ import numpy as np
 import tensorflow as tf
 from tensorflow import keras
 
-img = imread('../data/triforce.jpg')  # Grey-scale only for now
+#img = imread('../data/triforce.jpg')  # Grey-scale only for now
+img = np.array([[0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+                       [0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0],
+                       [0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0],
+                       [0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0],
+                       [0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0],
+                       [0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0],
+                       [0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0],
+                       [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+                       [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+                       [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                       [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]])
 
 # Rescale img's values to be [0 - 1]
 img = img / 255.
@@ -18,8 +29,8 @@ img = img / 255.
 train_X = np.random.random(size=img.shape)
 train_y = img
 # TensorFlow expects 5D tensors of kind (samples, time, rows, cols, channels)
-train_X = np.resize(train_X, (1, 1, 260, 260, 1))
-train_y = np.resize(train_y, (1, 1, 260, 260, 1))
+train_X = np.resize(train_X, (1, 1, img.shape[0], img.shape[1], 1))
+train_y = np.resize(train_y, (1, 1, img.shape[0], img.shape[1], 1))
 
 # Define a simple triangular kernel
 triangle = np.array([[0, 0, 0, 1, 0, 0, 0],
@@ -30,27 +41,41 @@ triangle = np.array([[0, 0, 0, 1, 0, 0, 0],
                      [1, 1, 1, 1, 1, 1, 1],
                      [1, 1, 1, 1, 1, 1, 1]])
 
-kernel_t = tf.constant_initializer(triangle)
+triangle_2 = np.array([[0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+                       [0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0],
+                       [0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0],
+                       [0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0],
+                       [0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0],
+                       [0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0],
+                       [0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0],
+                       [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+                       [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+                       [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                       [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]])
+
+kernel_t = tf.constant_initializer(triangle_2)
 
 # Define a simple LSTM model
 model = keras.Sequential([
         keras.layers.ConvLSTM2D(
-                 input_shape=(None, 260, 260, 1),
-                 filters=7,
-                 kernel_size=(7, 7),
-                 padding='same',
+             input_shape=(None, train_X.shape[2], train_X.shape[3], 1),
+             filters=1,
+             kernel_size=(7, 7),
+             padding='same',
 #                 strides=1,
-#                 dilation_rate=(1, 1),
-                 activation='relu',
+#                 dilation_rate=(10, 10),
+             activation='relu',
 #                 recurrent_activation='relu',
-                 kernel_initializer=kernel_t,
+#                 use_bias=True,
+             kernel_initializer=kernel_t,
 #                 recurrent_initializer=None,
 #                 kernel_constraint=None,
-                 return_sequences=True,
+             return_sequences=True,
 #                 return_state=False,
 #                 stateful=True,
-                 data_format='channels_last'),
-        keras.layers.Lambda(lambda x: x/255)
+             data_format='channels_last'),
+        keras.layers.Lambda(lambda x: x/255),
+
 #        keras.layers.BatchNormalization()
 #HINT   keras.layer.Permute(...)  # To apply the different kernels?
         ])
@@ -61,13 +86,13 @@ model.compile(optimizer='adadelta',  # ???Why Adadelta?
               metrics=['mean_squared_error'])
 
 # Fit the model to the training image
-model.fit(train_X, train_y, epochs=2, verbose=2, batch_size=1)
+model.fit(train_X, train_y, epochs=5, verbose=2, batch_size=1)
 
 # Evaluate model
 # TODO model.evaluate(...)
 
 # Predict a random array for testing
-test_X = np.resize(np.random.random(size=img.shape), (1, 1, 260, 260, 1))
+test_X = np.resize(np.random.random(size=img.shape), (1, 1, img.shape[0], img.shape[1], 1))
 pred_y = model.predict(test_X)
 pred_y_img = pred_y[0, 0, :, :, :]
 imshow(pred_y_img[:, :, 0])
