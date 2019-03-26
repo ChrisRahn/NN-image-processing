a4ae6e490425a9ff466294f14a3eb31185d3e6cb
Chris Rahn <crahn2011@gmail.com>
2019-03-26 14:58:42 -0700

    Smaller Kernel
    Better resolution

--- src/trainer.py
+++ src/trainer.py
@@ -21,9 +21,10 @@ img = simple_tri()
 train_X = simple_tri()
 train_y = np.array([[5, 5, 11]])
 # TensorFlow expects 5D tensors of kind (batch_size, rows, cols, channels)
-train_X = np.resize(train_X, (1, train_X.shape[0], train_X.shape[1], 1))
 IN_SHAPE = train_X.shape
 OUT_SHAPE = train_y.shape
+train_X = np.resize(train_X, (1, train_X.shape[0], train_X.shape[1], 1))
+
 # Define a simple triangular kernel
 tiny_tri = np.array([[0, 1, 0],
                      [0, 1, 0],
@@ -39,7 +40,7 @@ triangle = np.array([[0, 0, 0, 1, 0, 0, 0],
 
 triangle_2 = simple_tri()
 
-kernel_t = tf.constant_initializer(triangle_2)
+kernel_t = tf.constant_initializer(tiny_tri)
 
 
 def my_rescaler(X):
@@ -51,14 +52,14 @@ def my_rescaler(X):
 model = keras.Sequential([
         # Maxpool the image
         keras.layers.MaxPool2D(
-            input_shape=(train_X.shape[1], train_X.shape[2], 1),
-            pool_size=(26, 26),
+            input_shape=(IN_SHAPE[0], IN_SHAPE[1], 1),
+            pool_size=(1, 1),
             padding='same',
             data_format='channels_last'),
         # Convolve the pooled image by the shape kernel(s)
         keras.layers.Conv2D(
             filters=1,
-            kernel_size=(11, 11),
+            kernel_size=(3, 3),
             strides=(1, 1),
             padding='same',
             data_format='channels_last',
@@ -66,18 +67,19 @@ model = keras.Sequential([
             # use_bias=True,
             kernel_initializer=kernel_t),
         # Basic Dense layer
-        keras.layers.Dense(
-            units=121,
-            activation='relu',
-            use_bias=True),
-        # Basic Dense layer
+#        keras.layers.Dense(
+#            units=121,
+#            activation='relu',
+#            use_bias=True),
+        # Output layer
         keras.layers.Dense(
             units=3,
             activation='relu',
             use_bias=True),
+        #keras.layers.Flatten(),
         # Reshape down to the acutal features
-        keras.layers.Reshape(
-            target_shape=train_y.shape)
+#        keras.layers.Reshape(
+#            target_shape=OUT_SHAPE)
         ])
 
 # Compile the model
