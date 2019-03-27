06ff4f25fccd147e15a7d7eac608d6c4f61476d5
Chris Rahn <crahn2011@gmail.com>
2019-03-27 12:13:20 -0700

    Std Triangle OK
    train_gen draws the triangle template

--- src/train_gen.py
+++ src/train_gen.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Creates a training set for the model
+Generator script for training image-bundles
 """
 import numpy as np
 import math
 import cairo
+
 # Simple arrays for testing
 
 
@@ -45,40 +46,68 @@ class ImageBundle():
 
 
 class CustomImage():
-    def __init__(self):
-        self.WIDTH, self.HEIGHT = 512, 512
+    def __init__(self, width, height):
+        self.WIDTH, self.HEIGHT = width, height
         # Create empty shapelist
         # Each row repr one shape with [pos_x, pos_y, w_scale, h_scale, rot]
-        self.triangles = np.zeros((512, 512))
-        # ??? Store a separate array of shape types
-        # self.shape_types =
+        self.triangles = None
+
+        # ??? Store a separate array of shape types?
+        # self.shape_types = ...
+
+        # Initialize an array of white RGBA values (4 channel)
+        self.img = 255 * np.zeros((self.WIDTH, self.HEIGHT, 4), dtype=np.uint8)
         # Initialize a Cairo context and drawing surface
-        self.img = np.ndarray()
-        self.surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, 512, 512)
-        self.ctx = cairo.Context(surface)
-        self.ctx.set_source_rgb(1, 1, 1)
+        self.surface = cairo.ImageSurface.create_for_data(
+            self.img,
+            cairo.FORMAT_ARGB32,
+            self.WIDTH, self.HEIGHT)
+        self.ctx = cairo.Context(self.surface)
+        self.ctx.set_source_rgb(1.0, 0.0, 1.0)
         self.ctx.paint()
-        self.ctx.set_source_rgb(0, 0, 0)
 
     def triangle(self):
-        # Create just the path for a basic equilateral triangle
-        self.ctx.scale(self.WIDTH, self.HEIGHT)
-        self.ctx.move_to(0.50*WIDTH, 0.17*HEIGHT)
-        self.ctx.line_to(0.75*WIDTH, 0.625*HEIGHT)
-        self.ctx.line_to(0.25*WIDTH, 0.625*HEIGHT)
-        self.ctx.close_path()
-        self.ctx.fill()
+        '''Create a basic equilateral triangle
+           Middle-centered w/ side length 0.5*WIDTH'''
+        WIDTH, HEIGHT = self.WIDTH, self.HEIGHT
+        ctx = self.ctx
+        ctx.identity_matrix()
+        ctx.move_to(0.50*WIDTH, 0.17*HEIGHT)
+#        ctx.rel_line_to(0.5*math.cos(-math.pi/3),
+#                        0.5*math.sin(-math.pi/3))
+#        ctx.rel_line_to(-0.5, 0)
+        ctx.line_to(0.75*WIDTH, 0.625*HEIGHT)
+        ctx.line_to(0.25*WIDTH, 0.625*HEIGHT)
+        ctx.close_path()
 
-    def create_image(self):
-        # Create a standard triangle on the drawing surface
+    def draw_tri(self, num_tri):
+        ''' Draw random triangles using triangle() as a template'''
+        WIDTH, HEIGHT = self.WIDTH, self.HEIGHT
+        ctx = self.ctx
+        self.triangles = np.empty((num_tri, 5))
+        ctx.set_source_rgb(0.0, 1.0, 0.0)
+        for i in range(num_tri):
+            ctx.identity_matrix()  # Reset the drawing transformation
 
+            # Randomize drawing params
+            off_x = 0.5 - np.random.rand()
+            off_y = 0.5 - np.random.rand()
+            w_scale = WIDTH * (np.random.rand() + 0.001)
+            h_scale = HEIGHT * (np.random.rand() + 0.001)
+            rot = 2 * math.pi * np.random.rand()
+
+            # Set drawing transformation, then stamp triangle template
+#            ctx.scale(WIDTH, HEIGHT)
+#            ctx.rotate(rot)
+#            ctx.translate(off_x, off_y)
+            self.triangle()
+            ctx.fill()
+            self.triangles[i, :] = [off_x, off_y, w_scale, h_scale, rot]
 
 
 if (__name__ == '__main__'):
-    surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, width, height)
-    ctx = cairo.Context()
-    # TODO Drawing functions...
-    buf = surface.get_data()
-    data = np.ndarray(shape=(width, height),
-                     dtype=np.uint32,
-                     buffer=buf)
+    test = CustomImage(512, 512)
+    test.draw_tri(1)
+    print(test.img[:, :, 0])
+    test.surface.write_to_png('test.png')
+    print(test.triangles)
