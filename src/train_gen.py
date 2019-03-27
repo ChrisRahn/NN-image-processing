bbcf3206a6273e8e2ebf4a4446db503384a7b6ac
Chris Rahn <crahn2011@gmail.com>
2019-03-27 10:03:17 -0700

    Cairo Objects & Methods
    Beginnings of cairo objects to generate training data

--- src/train_gen.py
+++ src/train_gen.py
@@ -4,6 +4,7 @@
 Creates a training set for the model
 """
 import numpy as np
+import math
 import cairo
 # Simple arrays for testing
 
@@ -35,11 +36,42 @@ def simple_test():
                      [0.5, 1, 1, 1, 0.5, 0, 0.5, 1, 1, 1, 0.5],
                      [1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1]])
 
-    
-class drawing_bundle():
+
+class ImageBundle():
     def __init__(self):
-        # Create empty shapelists like (index, pos_x, pos_y, w_scale, h_scale, )
-        self.shapelist=np.zeros(())
+        # Individual images are array objects
+        # Bundle images together as (img_idx, num_shapes, shape_attr)
+        self.images = np.empty_like((5, 3, 5))
+
+
+class CustomImage():
+    def __init__(self):
+        self.WIDTH, self.HEIGHT = 512, 512
+        # Create empty shapelist
+        # Each row repr one shape with [pos_x, pos_y, w_scale, h_scale, rot]
+        self.triangles = np.zeros((512, 512))
+        # ??? Store a separate array of shape types
+        # self.shape_types =
+        # Initialize a Cairo context and drawing surface
+        self.img = np.ndarray()
+        self.surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, 512, 512)
+        self.ctx = cairo.Context(surface)
+        self.ctx.set_source_rgb(1, 1, 1)
+        self.ctx.paint()
+        self.ctx.set_source_rgb(0, 0, 0)
+
+    def triangle(self):
+        # Create just the path for a basic equilateral triangle
+        self.ctx.scale(self.WIDTH, self.HEIGHT)
+        self.ctx.move_to(0.50*WIDTH, 0.17*HEIGHT)
+        self.ctx.line_to(0.75*WIDTH, 0.625*HEIGHT)
+        self.ctx.line_to(0.25*WIDTH, 0.625*HEIGHT)
+        self.ctx.close_path()
+        self.ctx.fill()
+
+    def create_image(self):
+        # Create a standard triangle on the drawing surface
+
 
 
 if (__name__ == '__main__'):
