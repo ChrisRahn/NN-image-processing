bccc6dcfc0ee8ebe87a51e7866efa860214268cf
Chris Rahn <crahn2011@gmail.com>
2019-03-27 15:10:44 -0700

    Refactor for New Training

--- src/trainer.py
+++ src/trainer.py
@@ -10,83 +10,69 @@ import tensorflow as tf
 from tensorflow import keras
 import keras.backend as K
 from tensorflow.keras.constraints import min_max_norm, non_neg
-from train_gen import simple_tri, simple_test
+import kernels
+from train_gen import CustomImage, ImageBundle
+import pickle
 
-img = imread('../data/triforce.jpg')  # Grey-scale only for now
-#img = simple_tri()
+# Load the training set from the pickled ImageBundle
+train_bundle = pickle.load(open('../data/train_set_01.pkl', 'rb'))
+train_X = train_bundle.images
+train_y = train_bundle.tri_list
 
-# Rescale img's values to be [0 - 255]
-# img = img * 255.  #TODO Remove prob
+BATCH_SIZE = train_X.shape[0]
+# IN: (rows, cols, channels)
+IN_SHAPE = (train_X.shape[1], train_X.shape[2], train_X.shape[3])
+# OUT: (shape_idx, shape_attr)
+OUT_SHAPE = (train_y.shape[1], train_y.shape[2])
 
-# Set training data to be random and test to be the image
-train_X = imread('../data/dummy_train.jpg')
-train_y = np.array([[200, 150, 160]])
-# TensorFlow expects 5D tensors of kind (batch_size, rows, cols, channels)
-IN_SHAPE = train_X.shape
-OUT_SHAPE = train_y.shape
-train_X = np.resize(train_X, (1, train_X.shape[0], train_X.shape[1], 1))
-
-# Define a simple triangular kernel
-tiny_tri = np.array([[0, 1, 0],
-                     [0, 1, 0],
-                     [1, 0, 1]])
-
-triangle = np.array([[0, 0, 0, 1, 0, 0, 0],
-                     [0, 0, 1, 1, 1, 0, 0],
-                     [0, 0, 1, 1, 1, 0, 0],
-                     [0, 1, 1, 1, 1, 1, 0],
-                     [0, 1, 1, 1, 1, 1, 0],
-                     [1, 1, 1, 1, 1, 1, 1],
-                     [1, 1, 1, 1, 1, 1, 1]])
-
-triangle_2 = simple_tri()
-
-kernel_t = tf.constant_initializer(triangle_2)
+# Define a simple triangular kernel and kernel constraints
+kernel_tri = tf.constant_initializer(kernels.triangle_5())
 kernel_const = min_max_norm(0.001, None, rate=1, axis=0)
 kernel_nonneg = non_neg()
 
-
-def my_rescaler(X):
-    max_val = K.max(K.max(X, axis=2, keepdims=True), axis=3)[0, 0, 0, 0]
-    min_val = K.min(K.min(X, axis=2, keepdims=True), axis=3)[0, 0, 0, 0]
-    return 255*(X - min_val)/(max_val - min_val)
-
-
+# TensorFlow expects 4D tensors of shape (batch_size, rows, cols, channels)
+# Note that the first index (the sample index out of the batch) is stripped
 model = keras.Sequential([
+
         # Maxpool the image
         keras.layers.MaxPool2D(
-            input_shape=(IN_SHAPE[0], IN_SHAPE[1], 1),
+            input_shape=IN_SHAPE,
             pool_size=2,
             padding='same',
             data_format='channels_last'),
+
         # Convolve the pooled image by the shape kernel(s)
+        # ??? Use LocallyConnected2D instead?
         keras.layers.Conv2D(
-            filters=1,
-            kernel_size=(11, 11),
-            strides=(1, 1),
+            filters=5,
+            kernel_size=(16, 16),
+            strides=(16, 16),
             padding='same',
             data_format='channels_last',
             activation='sigmoid',
-            # use_bias=True,
-            kernel_initializer=kernel_t,
-            kernel_constraint=kernel_nonneg),
+            use_bias=True),
+            # ??? kernel_initializer=kernel_tri,
+            # kernel_constraint=kernel_nonneg),
+
+        # Flatten
         keras.layers.Flatten(),
+
         # Basic Dense layer
         keras.layers.Dense(
-            units=81,
+            units=512,
             activation=None,
-            kernel_constraint=kernel_nonneg,
+            # kernel_constraint=kernel_nonneg,
             use_bias=True),
-        # Output layer
+
+        # Another Dense layer
         keras.layers.Dense(
-            units=3,
+            units=5,
             activation=None,
-            kernel_constraint=kernel_nonneg,
+            # kernel_constraint=kernel_nonneg,
             use_bias=True),
-        keras.layers.PReLU(),
-        # Reshape down to the acutal features
-#        keras.layers.Reshape(
-#            target_shape=OUT_SHAPE)
+
+        # Output layer
+        keras.layers.PReLU()
         ])
 
 # Compile the model
@@ -95,16 +81,16 @@ model.compile(optimizer='adadelta',
               metrics=['mean_squared_error'])
 
 # Fit the model to the training image
-model.fit(train_X, train_y, epochs=100, verbose=1, batch_size=1)
+model.fit(train_X, train_y, epochs=2, verbose=1, batch_size=5)
 
 # Evaluate model
 # TODO model.evaluate(...)
 
 # Repredict the training img
-test_X = 255 - imread('../data/triforce.jpg')
-test_X = np.reshape(test_X, (1, test_X.shape[0], test_X.shape[1], 1))
-y_pred = model.predict(test_X)
-imshow(test_X[0, :, :, 0])
-print('Trained shape data: ', train_y)
-print('Determined shape data: ', y_pred)
-y_pred
+#test_X = 255 - imread('../data/triforce.jpg')
+#test_X = np.reshape(test_X, (1, test_X.shape[0], test_X.shape[1], 1))
+#y_pred = model.predict(test_X)
+#imshow(test_X[0, :, :, 0])
+#print('Trained shape data: ', train_y)
+#print('Determined shape data: ', y_pred)
+#y_pred
