ffd5c5bc74a8cb2da2fa2d4c74e338706651dc8c
Chris Rahn <crahn2011@gmail.com>
2019-04-05 11:20:55 -0700

    Adapt Prediction for Lines

--- src/teller.py
+++ src/teller.py
@@ -19,7 +19,7 @@ if (__name__ == '__main__'):
     try:
         NUM_SHAPES = int(sys.argv[2])
     except IndexError:
-        NUM_SHAPES = input('How many shapes do you want?')
+        NUM_SHAPES = int(input('How many shapes do you want?'))
 
     MODEL_PATH = input('Which saved model should I use?')
     model = keras.models.load_model(
@@ -30,24 +30,21 @@ if (__name__ == '__main__'):
     img_in = InputImage(IMAGE_PATH)
 
     # Reshape to a 1-page batch and feed through model
-    model_feed = img_in.data.reshape(1, 512, 512, 1)  # TODO Allow sizes
-    model_out = model.predict(model_feed)  # [2xpos, 2xsiz, 1xrot]
+    model_feed = img_in.data.reshape(1, 256, 256, 1)
+    model_out = model.predict(model_feed)  # [x1, y1, x2, y2]
 
     # Concatenate model outputs to one array
-    pred_pos = model_out[0][0, :, :]  # 30x2 position array
-    pred_siz = model_out[1][0, :, :]  # 30x2 size array
-    pred_rot = model_out[2][0, :].reshape(30, 1)  # 30x1 rotation array
-    shapes_out = np.hstack((pred_pos, pred_siz, pred_rot))
+    shapes_out = model_out[0, 0, :, :, :]
 
     # Filter to the number of shapes desired (NUM_SHAPES)
     print(NUM_SHAPES)
-    shapes_filt = shapes_out[:NUM_SHAPES, :]
+    shapes_filt = shapes_out[:NUM_SHAPES, :, :]
 
     # Print the numerical output to the console
     print(shapes_filt)
 
     # Wrap the raw output in the OutputImage class
-    img_out = OutputImage(512, 512, shapes_filt)  # TODO sizes
+    img_out = OutputImage(256, 256, lines=shapes_filt)
 
     # Display both the input and output image
     img_in.display()
