3c9112732b701ff181505331d40626b8a16ea6aa
Chris Rahn <crahn2011@gmail.com>
2019-04-26 21:12:21 -0700

    Env Initializes OK

---
+++ gym-kame/debug.py
@@ -0,0 +1,4 @@
+if (__name__ == "__main__"):
+    import gym
+    import gym_kame
+    env = gym.make('kame-v0')
--- gym-kame/gym_kame/envs/kame_env.py
+++ gym-kame/gym_kame/envs/kame_env.py
@@ -2,60 +2,87 @@ import gym
 from gym import error, spaces, utils
 from gym.utils import seeding
 import numpy as np
+import itertools
 
 class KameEnv(gym.Env):
     metadata = {'render.modes': ['human']}
     FPS = 10
-    TURTLE_POLY = [
-        (0, +10), (-4, -4), (+4, -4)
-        ]
 
     def __init__(self):
         self.seed()
         self.viewer = None
+        self.state = None
+        self.tau = 0.3  # seconds b/w state updates
         
-        self.grid_width = 2
-        self.grid_height = 2
+        self.grid_width = 3
+        self.grid_height = 3
         self.num_pix = self.grid_width*self.grid_height
-        self.pos = (1, 1)
-        self.speed = 1
+        self.pos = np.array([[1, 1]])
         self.distance_tot = 0
         
-        self.grid = np.array([[1, 1],
-                              [1, 1]])
+        self.grid = self.np_random.randint(2, size=(self.grid_height, self.grid_width))
 
         # Observation space is the grid of pixels
-        self.observation_space = spaces.MultiBinary(n=self.num_pix)
+        self.observation_space = spaces.Tuple((spaces.Discrete(2), spaces.Discrete(2), spaces.MultiBinary(n=self.num_pix)))
         # Action space is tuple of two ints [right, up]
-        # right: the x-coord of the turtle's next position, 0..grid_width
-        # up: the y-coord of the turtle's next position, 0..grid_height
+        # right: the x-coord of the nib's next position, 0..grid_width
+        # up: the y-coord of the nib's next position, 0..grid_height
         self.action_space = spaces.Tuple((spaces.Discrete(self.grid_width), spaces.Discrete(self.grid_height)))
 
         self.prev_reward = None
 
         self.reset()
+
+        self.observation = tuple(itertools.chain(self.pos, self.grid.flatten()))
+        # assert self.observation_space.contains(self.observation), "The observation was %r, should be like %s" % (self.observation, observation_space.sample())
         
     def seed(self, seed=None):
         self.np_random, seed = seeding.np_random(seed)
         return [seed]
 
-    def step(self, action):
-        assert self.action_space.contains(action), "%r (%s) invalid" % (action, type(action))
+    def step(self, next_pos):
+        assert self.action_space.contains(next_pos), "%r (%s) invalid" % (next_pos, type(next_pos))
 
-        self.distance_tot += np.sqrt(sum([(i-j)**2 for i,j in zip(self.pos, action)]))
-        self.pos = action
+        delta = np.array([(i-j) for i,j in zip(next_pos, self.pos)])
+        self.distance_tot += np.sqrt(delta.sum())
+        
+        while any(delta):
+            next_step = np.sign(delta)
+            self.pos += next_step # step one pixel in the direction of the next point
+            reward += self.grid[self.pos]  # increase the reward if the pixel is black
+            self.grid[self.pos] = 0  # blank the pixel
+            delta -= next_step  # Decrement the delta
 
-        done = False # TODO
-        reward = -1.0
+        done = False # TODO # stopping
+        reward += -0.5  # reward penalty per step
 
-        return self.pos, reward, done, {}
+        new_state = tuple(itertools.chain(self.pos, self.grid.flatten()))
+        return new_state, reward, done, {}
 
     
     def reset(self):
-        pass
+        res_posx, res_posy = self.np_random.randint(2, size=2)
+        res_grid = self.np_random.randint(2, size=(self.grid_height, self.grid_width))
+        self.state = tuple(itertools.chain([res_posx, res_posy], res_grid.flatten()))
+        return self.state
     
     def render(self, mode='human'):
-        from gym.envs.classic_control import rendering
-    
+        screen_width = 400
+        screen_height = 400
+        pix_width = screen_width/grid_width
+        pix_height = screen_height/grid_height
+
+        if self.viewer is None:  # Build the viewer
+            from gym.envs.classic_control import rendering
+            self.viewer = rendering.Viewer(screen_width, screen_height)
+            nib = rendering.make_circle(radius=10, res=30, filled=True)
+            nib.set_color(1.0, 1.0, 1.0)
+            self.viewer.add_geom(nib)
+            self._nib_geom = nib
+        
+        if self.state is None: return None
+        
+        return self.viewer.render(return_rgb_array= mode=='rgb_array')
+
     def close(self):
         pass
